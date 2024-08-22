6cabd9360f56e957a7a78550c9d23440117525df
Lets-Go-Champ <christian.hiley@gmail.com>
2024-08-22 15:56:14 +1000

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.cab302assessment10b0101 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.cab302assessment10b0101 to javafx.fxml;
+    exports com.example.cab302assessment10b0101;
+}
