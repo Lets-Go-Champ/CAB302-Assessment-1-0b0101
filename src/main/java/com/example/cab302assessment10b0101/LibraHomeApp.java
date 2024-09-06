6100aa0ba3d85cf9905d5c61ae8ad520c53da0c6
Liz <elishy.qian@gmail.com>
2024-09-06 21:35:21 +1000

    add macOS-like background

--- src/main/java/com/example/cab302assessment10b0101/LibraHomeApp.java
+++ src/main/java/com/example/cab302assessment10b0101/LibraHomeApp.java
@@ -9,8 +9,10 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 
 public class LibraHomeApp extends Application {
@@ -20,17 +22,17 @@ public class LibraHomeApp extends Application {
         // Load image from resources
         Image image = new Image(getClass().getResource("/com/example/cab302assessment10b0101/download.png").toExternalForm());
         ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(400); // Set to half the width of the screen
+        imageView.setFitWidth(250); // Set to half the width of the screen
         imageView.setPreserveRatio(true);
 
         // Username and Password fields with placeholders
         TextField usernameField = new TextField();
         usernameField.setPromptText("Enter Username");
-        usernameField.setMaxWidth(250);
+        usernameField.setMaxWidth(200);
 
         PasswordField passwordField = new PasswordField();
         passwordField.setPromptText("Enter Password");
-        passwordField.setMaxWidth(250);
+        passwordField.setMaxWidth(200);
 
         // Creating buttons for login and account creation
         Button loginButton = new Button("Login");
@@ -43,16 +45,25 @@ public class LibraHomeApp extends Application {
         // Arranging elements in a VBox
         VBox vbox = new VBox(10);
         vbox.setAlignment(Pos.CENTER);
-        vbox.setPadding(new Insets(20));
+        vbox.setPadding(new Insets(10));
         vbox.getChildren().addAll(usernameField, passwordField, loginButton, createAccountButton);
 
         // HBox to divide the screen into left (image) and right (login)
-        HBox hbox = new HBox(20);
+        HBox hbox = new HBox(10);
         hbox.getChildren().addAll(imageView, vbox);
         hbox.setAlignment(Pos.CENTER);
 
+        // Background gradient
+        Stop[] stops = new Stop[] {
+                new Stop(0, Color.web("#dfe9f3")),
+                new Stop(1, Color.web("#ffffff"))
+        };
+        LinearGradient macBackground = new LinearGradient(0, 0, 1, 1, true, null, stops);
+
+        hbox.setBackground(new Background(new BackgroundFill(macBackground, CornerRadii.EMPTY, Insets.EMPTY)));
+
         // Setting up the scene and stage
-        Scene scene = new Scene(hbox, 800, 600);
+        Scene scene = new Scene(hbox, 600, 400);
         primaryStage.setTitle("LibraHome - Home Library Catalogue");
         primaryStage.setScene(scene);
         primaryStage.show();
