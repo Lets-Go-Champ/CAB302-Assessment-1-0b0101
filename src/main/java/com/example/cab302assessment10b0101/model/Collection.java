c8e8fb0fb21dd1183f10dfa2db2e9b58201e4a83
Lets-Go-Champ <christian.hiley@gmail.com>
2024-09-11 13:13:24 +1000

    Collection class

--- src/main/java/com/example/cab302assessment10b0101/model/Collection.java
+++ src/main/java/com/example/cab302assessment10b0101/model/Collection.java
@@ -1,4 +1,32 @@
 package com.example.cab302assessment10b0101.model;
 
 public class Collection {
+    private int id;
+    private String collectionName;
+    private String description;
+
+    public Collection(String collectionName, String description) {
+        this.collectionName = collectionName;
+        this.description = description;
+    }
+
+    public int getId() {return id; }
+
+    public void setId(int id) {this.id = id;}
+
+    public String getCollectionName() {
+        return collectionName;
+    }
+
+    public void setCollectionName(String collectionName) {
+        this.collectionName = collectionName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
