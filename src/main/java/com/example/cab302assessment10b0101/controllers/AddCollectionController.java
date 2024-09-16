8d3f2fc7a55e55cb52a4784f0795ee823165685e
Lets-Go-Champ <christian.hiley@gmail.com>
2024-09-17 09:14:50 +1000

    Updated controller fields and completed view for Add collection

--- src/main/java/com/example/cab302assessment10b0101/controllers/AddCollectionController.java
+++ src/main/java/com/example/cab302assessment10b0101/controllers/AddCollectionController.java
@@ -1,4 +1,29 @@
 package com.example.cab302assessment10b0101.controllers;
 
-public class AddCollectionController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCollectionController implements Initializable {
+
+    @FXML
+    private TextField CollectionNameTextfield;
+
+    @FXML
+    private TextField CollectionDescriptionTextFiled;
+
+    @FXML
+    private Button SaveBtn;
+
+    @FXML
+    private Button CancelBtn;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
+    }
 }
