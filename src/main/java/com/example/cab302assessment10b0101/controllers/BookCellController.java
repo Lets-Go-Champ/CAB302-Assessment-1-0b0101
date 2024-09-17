79985443a49351a1664b87ebe8c10e1a8b637f3b
Lets-Go-Champ <christian.hiley@gmail.com>
2024-09-17 16:02:47 +1000

    Initialise book cell and controller

---
+++ src/main/java/com/example/cab302assessment10b0101/controllers/BookCellController.java
@@ -0,0 +1,13 @@
+package com.example.cab302assessment10b0101.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookCellController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+    }
+}
