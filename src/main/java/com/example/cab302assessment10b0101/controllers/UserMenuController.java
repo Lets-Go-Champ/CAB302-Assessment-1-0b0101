09a9b0b413c807ee593a7a48ac6d03819ca7ce08
Lets-Go-Champ <christian.hiley@gmail.com>
2024-09-20 15:24:31 +1000

    Updated logout

--- src/main/java/com/example/cab302assessment10b0101/controllers/UserMenuController.java
+++ src/main/java/com/example/cab302assessment10b0101/controllers/UserMenuController.java
@@ -47,7 +47,6 @@ public class UserMenuController implements Initializable {
         //ViewManager.getInstance().getViewFactory().getLoginScreen();
         //UserManager.getInstance().setCurrentUser(null);
         showLogoutSuccessAlert();
-
     }
     private void showLogoutSuccessAlert() {
         Alert alert = new Alert(AlertType.CONFIRMATION);
