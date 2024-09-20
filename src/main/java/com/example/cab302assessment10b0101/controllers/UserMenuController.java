7f0777d381105a22de4754980cf15b44982e505c
Lets-Go-Champ <christian.hiley@gmail.com>
2024-09-20 13:51:59 +1000

    Updated Logout Functionality

--- src/main/java/com/example/cab302assessment10b0101/controllers/UserMenuController.java
+++ src/main/java/com/example/cab302assessment10b0101/controllers/UserMenuController.java
@@ -1,14 +1,18 @@
 package com.example.cab302assessment10b0101.controllers;
+import com.example.cab302assessment10b0101.model.UserManager;
 import com.example.cab302assessment10b0101.views.MenuOptions;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import com.example.cab302assessment10b0101.model.ViewManager;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 import java.net.URL;
@@ -39,16 +43,35 @@ public class UserMenuController implements Initializable {
         ViewManager.getInstance().getViewFactory().getUserSelectedMenuItem().set(MenuOptions.ADDCOLLECTION);
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle){
-       // addListeners();
-    }
 
     @FXML
-    private void onLogoutClicked() {
-        showLogoutConfirmation();
+    private void onLogoutClicked(){
+        Stage stage = (Stage) myBooksBtn.getScene().getWindow();
+        ViewManager.getInstance().getViewFactory().closeStage(stage);
+        ViewManager.getInstance().getViewFactory().getLoginScreen();
+        UserManager.getInstance().setCurrentUser(null);
+        showLogoutSuccessAlert();
+
+    }
+    private void showLogoutSuccessAlert() {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Logout Successful");
+        alert.setHeaderText(null);  // Optional: Set to null if you don't want a header
+        alert.setContentText("You have successfully logged out.");
+
+        // Display the alert and wait for the user to close it
+        alert.showAndWait();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+
     }
 
+    //@FXML
+    //private void onLogoutClicked() {
+        //showLogoutConfirmation();
+    //}
+/*
     private void showLogoutConfirmation() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/cab302assessment10b0101/fxml/LogoutConfirmation.fxml"));
@@ -62,4 +85,6 @@ public class UserMenuController implements Initializable {
             e.printStackTrace();
         }
     }
+    */
+
 }
