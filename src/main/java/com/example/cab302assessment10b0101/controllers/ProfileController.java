d35f0f7827c1ffab1fc92215196ef0b13aceca1d
Alex <wattsiet@gmail.com>
2024-09-25 13:50:08 +1000

    Created Profile controller and fxml

---
+++ src/main/java/com/example/cab302assessment10b0101/controllers/ProfileController.java
@@ -0,0 +1,15 @@
+package com.example.cab302assessment10b0101.controllers;
+
+import javafx.scene.control.TextField;
+
+public class ProfileController {
+
+    public TextField usernameTextField;
+    public TextField passwordTextField;
+
+    public void handleUpdateUsername() {
+    }
+
+    public void handleUpdatePassword() {
+    }
+}
