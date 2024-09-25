b70d17e1d9ca3ce560ba3fc856f3251a59f7a431
Alex <wattsiet@gmail.com>
2024-09-25 15:43:22 +1000

    Added a controller and fxml for updating a username.

---
+++ src/main/java/com/example/cab302assessment10b0101/controllers/ChangeUsernameController.java
@@ -0,0 +1,14 @@
+package com.example.cab302assessment10b0101.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ChangeUsernameController {
+
+    @FXML
+    private TextField newUsernameTextField;
+
+    public void handleUpdateUsername() {}
+
+    public void handleCancel() {}
+}
