4c46f45ec5fa9809d8b162db4b1755f7629a9b8f
quatermaster <j.mapson@yahoo.com.au>
2024-10-09 12:31:48 +1000

    All Test functionality complete (also remembered to push old commits)

--- src/main/java/com/example/cab302assessment10b0101/model/LoanDAOInterface.java
+++ src/main/java/com/example/cab302assessment10b0101/model/LoanDAOInterface.java
@@ -1,18 +1,42 @@
 package com.example.cab302assessment10b0101.model;
 
-import java.util.List;
+import javafx.collections.ObservableList;
 
-
+/**
+ * The LoanDAOInterface defines the contract for data access operations
+ * related to Loan objects. It specifies methods for inserting new loans,
+ * retrieving loans by user, deleting loans, and finding loan IDs by user and book.
+ */
 public interface LoanDAOInterface {
 
     /**
-     * Inserts a new loan into the database.
+     * Inserts a new loan into the data store.
      *
-     * @param loan The Collection object to be inserted.
+     * @param loan The Loan object to be inserted.
      */
-    void insert(Loan loan);
+    void insertLoan(Loan loan);
 
+    /**
+     * Retrieves all loans associated with a specific user ID.
+     *
+     * @param userId The ID of the user whose loans to retrieve.
+     * @return An ObservableList of Loan objects for the specified user.
+     */
+    ObservableList<Loan> getAllLoansByUser(int userId);
 
-    List<Loan> getAll();
+    /**
+     * Deletes a specific loan from the data store.
+     *
+     * @param loan The Loan object to be deleted.
+     */
+    void deleteLoan(Loan loan);
 
+    /**
+     * Retrieves the loan ID by user and book.
+     *
+     * @param userId The ID of the user associated with the loan.
+     * @param bookId The ID of the book associated with the loan.
+     * @return The loan ID if found, otherwise -1.
+     */
+    int getLoanIdByUserAndBook(int userId, int bookId);
 }
